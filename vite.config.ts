5bfd627b40768beaa64c4c6b914d595e853a027a
mistraldmca <legal@mistraldmca.com>
2026-03-31 13:03:02 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+export default defineConfig({
+  plugins: [react()],
+})
